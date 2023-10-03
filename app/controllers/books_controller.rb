4c363ea6e1a9247ee3a4d41b11a45bc34cb90833
ItsukiOgawa <139129093+kimaguregurashi@users.noreply.github.com>
2023-10-03 10:58:47 +0000

    課題3コメント終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
+  
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!
 
   def show
     @book = Book.find(params[:id])
+    @book_new = Book.new
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
@@ -22,14 +27,12 @@ class BooksController < ApplicationController
   end
 
   def edit
-    @book = Book.find(params[:id])
      unless @book.user == current_user
     redirect_to books_path
      end
   end
 
   def update
-    @book = Book.find(params[:id])
     if @book.update(book_params)
       redirect_to book_path(@book), notice: "You have updated book successfully."
     else
@@ -38,12 +41,18 @@ class BooksController < ApplicationController
   end
 
   def destroy
-    @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
 
   private
+  
+  def ensure_correct_user
+      @book = Book.find(params[:id])
+      unless @book.user == current_user
+        redirect_to books_path
+      end
+  end
 
   def book_params
     params.require(:book).permit(:title, :body)
